29066cf4e299d209141c1a23679ce1d92b87b7de
Çؽú <qjdev@qq.com>
2016-03-22 21:54:52 +0800

    init

---
+++ TimeGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeGallery.Startup))]
+namespace TimeGallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
