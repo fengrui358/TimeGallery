faaa5500e047b0bd2e4b9bf1b7a523b05e25cd37
秦晋 <qjdev@qq.com>
2016-05-12 18:40:46 +0800

    拷贝Demo示例

---
+++ TimeGallery/Weixin/Download/CodeRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Senparc.Weixin.MP.AdvancedAPIs.QrCode;
+
+namespace TimeGallery.Weixin.Download
+{
+    public class CodeRecord
+    {
+        public string Key { get; set; }
+        public int QrCodeId { get; set; }
+        public CreateQrCodeResult QrCodeTicket { get; set; }
+        public string Version { get; set; }
+        public bool Used { get; set; }
+        public bool AllowDownload { get; set; }
+    }
+}
---
+++ TimeGallery/Weixin/Download/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimeGallery.Weixin.Download
+{
+    public class Config
+    {
+        public int QrCodeId { get; set; }
+        public List<string> Versions { get; set; }
+        public int DownloadCount { get; set; }
+    }
+}
