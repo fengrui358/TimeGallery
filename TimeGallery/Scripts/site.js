bd8dee7adfd54f22da2cefbf3c9d66b2fbe56d32
free_macPro <fengrui358@163.com>
2016-06-01 11:48:03 +0800

    漏提交js

---
+++ TimeGallery/Scripts/site.js
@@ -0,0 +1,13 @@
+﻿$.extend({
+    showWarning: function (msg) {
+        var warningMsg = "警告";
+        if (arguments.length === 1) {
+            warningMsg = msg;
+        }
+
+        $('.js_tooltips').show();
+        setTimeout(function () {
+            $('.js_tooltips').hide();
+        }, 3000);
+    }
+});
