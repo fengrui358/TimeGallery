50583cdbb1c5b5c79d83a7afaa0e9d6685541e2b
free_macPro <fengrui358@163.com>
2016-06-06 16:59:28 +0800

    增加内容分组表

--- MysqlDump/timegallery.sql
+++ MysqlDump/timegallery.sql
@@ -23,15 +23,16 @@ DROP TABLE IF EXISTS `content`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `content` (
-  `Id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `GalleryId` bigint(11) NOT NULL DEFAULT '0',
-  `Type` varchar(20) NOT NULL,
+  `Id` char(36) NOT NULL COMMENT '主键',
+  `ContentGroupId` char(36) NOT NULL COMMENT '内容分组的外键',
+  `Type` varchar(20) NOT NULL COMMENT 'mime类型',
   `Url` varchar(128) NOT NULL,
   `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Size` varchar(50) NOT NULL,
   `Description` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`Id`),
-  KEY `CreateTime` (`CreateTime`)
+  KEY `CreateTime` (`CreateTime`),
+  KEY `ContentGroupId` (`ContentGroupId`)
 ) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COMMENT='内容表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -41,8 +42,37 @@ CREATE TABLE `content` (
 
 LOCK TABLES `content` WRITE;
 /*!40000 ALTER TABLE `content` DISABLE KEYS */;
-INSERT INTO `content` VALUES (5,0,'image/jpeg','http://7xrp60.com1.z0.glb.clouddn.com/o_1afjh8qb611089eiano18kiqkn9.jpg','2016-04-05 23:39:48','73060',NULL),(4,0,'video/quicktime','http://7xrp60.com1.z0.glb.clouddn.com/o_1afjce9gkqrv1lhj1tos51dcad9.mov','2016-04-05 22:15:30','5273396',NULL),(3,0,'image/jpeg','http://7xrp60.com1.z0.glb.clouddn.com/o_1af6glmpit2u1e0gfah6hv1j2p9.jpg','2016-03-31 22:19:15','1191451',NULL);
+INSERT INTO `content` VALUES ('5','0','image/jpeg','http://7xrp60.com1.z0.glb.clouddn.com/o_1afjh8qb611089eiano18kiqkn9.jpg','2016-04-05 23:39:48','73060',NULL),('4','0','video/quicktime','http://7xrp60.com1.z0.glb.clouddn.com/o_1afjce9gkqrv1lhj1tos51dcad9.mov','2016-04-05 22:15:30','5273396',NULL),('3','0','image/jpeg','http://7xrp60.com1.z0.glb.clouddn.com/o_1af6glmpit2u1e0gfah6hv1j2p9.jpg','2016-03-31 22:19:15','1191451',NULL);
 /*!40000 ALTER TABLE `content` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contentgroup`
+--
+
+DROP TABLE IF EXISTS `contentgroup`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contentgroup` (
+  `Id` char(36) NOT NULL COMMENT '主键',
+  `GalleryId` bigint(20) NOT NULL COMMENT '所属相册外键',
+  `ImageCount` int(11) NOT NULL DEFAULT '0',
+  `VideoCount` int(11) NOT NULL DEFAULT '0',
+  `Date` datetime NOT NULL,
+  `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`),
+  KEY `GalleryId` (`GalleryId`),
+  KEY `Date` (`Date`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='按天分组内容，统计一天内容的基本信息';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contentgroup`
+--
+
+LOCK TABLES `contentgroup` WRITE;
+/*!40000 ALTER TABLE `contentgroup` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contentgroup` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -56,10 +86,10 @@ CREATE TABLE `gallery` (
   `Id` bigint(20) NOT NULL AUTO_INCREMENT,
   `Name` varchar(50) NOT NULL DEFAULT '0',
   `Description` varchar(400) DEFAULT '0',
-  `CoverUrl` varchar(50) DEFAULT '0',
+  `Cover` varchar(50) DEFAULT '0',
   `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='相册';
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='相册';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +98,6 @@ CREATE TABLE `gallery` (
 
 LOCK TABLES `gallery` WRITE;
 /*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
-INSERT INTO `gallery` VALUES (1,'好嘛','啊京华时报','http://7xrp60.com1.z0.glb.clouddn.com/o_1ake46aim7','2016-06-05 00:33:39');
 /*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -127,7 +156,6 @@ CREATE TABLE `user_gallery_rel` (
 
 LOCK TABLES `user_gallery_rel` WRITE;
 /*!40000 ALTER TABLE `user_gallery_rel` DISABLE KEYS */;
-INSERT INTO `user_gallery_rel` VALUES ('oIKlFw0yLVagA1nNfEegqP_2o6Bs',1,4);
 /*!40000 ALTER TABLE `user_gallery_rel` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -140,4 +168,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-05 22:34:45
+-- Dump completed on 2016-06-06 16:58:59
